489dfdbdd9e71a1a67700e62f3279f0ea62940dc
Jayway954 <69004031+Jayway954@users.noreply.github.com>
2026-05-07 21:34:18 -0400

    New starting point for resume files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Resume-
+The repo is a Resume project to showcase DevOps skills as I learn 
+
+Resources that are being deployed:
+Azure Resource Group
+Azure Storage Account
+Azure CDN
---
+++ main.tf
@@ -0,0 +1,51 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_group_name_prefix}-resource-group"
+  location = var.resource_group_location
+
+  tags = {
+    environment = "production"
+    project     = "resume-project"
+    Owner       = "Jalin Johnson"
+  }
+}
+
+# Generate random value for the storage account name
+resource "random_string" "storage_account_name" {
+  length  = 8
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  name = random_string.storage_account_name.result
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
+  static_website {
+    index_document = "index.html"
+  }
+
+  tags = {
+    environment = "production"
+    project     = "resume-project"
+    Owner       = "Jalin Johnson"
+  }
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source                 = "index.html"
+
+}
